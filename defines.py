4ae950977921dcf32db0f6e9099670495da55137
miloszewiczek <110734913+miloszewiczek@users.noreply.github.com>
2024-08-18 16:17:35 +0200

    Dodanie metod do klasy Experiment. Dodanie funkcjonalnosci wczytywania plikow

--- defines.py
+++ defines.py
@@ -3,6 +3,9 @@
 import gamry_parser
 from math import pi
 from tkinter.filedialog import askdirectory
+import os
+from glob import glob
+import re
 
 DTA_Parser = gamry_parser.GamryParser()
 Experiments = []
@@ -39,17 +42,86 @@ def print_hello():
 
 
 
+
+def log_modification(func):
+    def wrapper(obj, *args, **kwargs):
+        # Przed modyfikacją
+        print(f"Modifying {obj.Experiment_TAG}...")
+        
+        # Wywołanie oryginalnej funkcji
+        result = func(obj, *args, **kwargs)
+        
+        # Po modyfikacji
+        print(f"Modyfying {obj.Experiment_TAG}...DONE")
+        
+        return result
+    return wrapper
+
 class Experiment:
-    def __init__(self, experiment_type):
-        self.experiment_type = experiment_type
-        self.experiment_data = None
+    def __init__(self, tag, data, cycle_number):
+        
+        self.Experiment_TAG = tag
+        self.Data = data
+        self.Cycle_number = cycle_number
+    
+    def Modify(self):
+        '''Depending on the TAG of the experiment, different data modifications are performed'''
+        Exp_Tag = self.Experiment_TAG
+        match Exp_Tag:
+            
+            case "HER" | "OER":
+                #Modifications
+                pass
+            
+            case "ECSA" | "Electrochemical Surface Area":
+                
+                print('Jestem ECSA, Co mam zrobić?')
 
-def LoadFile():
-    DTA_file = DTA_Parser.load('test_file.DTA')
+            case "CHRONOA":
+                pass
+        pass
+
+    def ChangeUnits(self):
+        pass
+
+    def SaveToExcel(self):
+        pass
+
+    def PlotToRaport(self):
+        pass
+
+def GetFilesFromFolder(folder_path):
+    '''A function to get file paths of *.DTA files of a given folder. Also checks for empty files'''
+
+    DTA_files = glob(folder_path + '/*.DTA')
+    non_empty_DTA_files = [file for file in DTA_files if os.stat(file).st_size != 0]
+    print(non_empty_DTA_files)
+    return non_empty_DTA_files
+
+def LoadFile(file_path):
+    '''Function to load file from string, creating TAG and list of curves'''
+
+    DTA_Parser.load(file_path)
     File_Header = DTA_Parser.get_header()
-    Experiment_TAG = File_Header['TAG']
-    exp = Experiment(experiment_type=Experiment_TAG)
-    setattr(exp, 'elo', 'naura')
-    print(exp.elo)
-    return 
+    Curves_List = DTA_Parser.get_curves()
+    Experiment_TAG = File_Header['TITLE']
 
+
+    #CHECK CYCLE BASED ON NAME
+    Cycle_number = re.search('#[1-9]+_#', file_path)
+    if Cycle_number != None:
+        Cycle_number = Cycle_number.group(0)[1]
+    Exp = Experiment(Experiment_TAG,
+                     Curves_List,
+                     Cycle_number)
+    
+    #Appends the tag to a global variable to avoid iterating over every object in next steps
+    Experiments.append(Experiment_TAG)
+
+    return Exp
+
+    
+        
+
+    
+    
